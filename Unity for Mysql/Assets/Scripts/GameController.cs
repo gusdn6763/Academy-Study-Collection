4fa46dfcd54823de56c36c1333a9f1ba2d392bbb
gusdn6763 <gusdn6763@gmail.com>
2019-12-03 13:39:30 +0900

    save

---
+++ Unity for Mysql/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetScore());
+    }
+
+    IEnumerator GetScore()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://localhost:3000");
+        yield return www.SendWebRequest();
+        if(www.isNetworkError||www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+}
